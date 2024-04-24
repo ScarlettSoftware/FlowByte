4551ee1554afa41ce2289c6874b07c730a5728ca
Ch1ngz <90413853+Ch1nChun@users.noreply.github.com>
2024-04-24 18:34:32 +0300

    Update Games.h

--- FlowByte/Tabs/Games.h
+++ FlowByte/Tabs/Games.h
@@ -1,5 +1,7 @@
 #include <iostream>
 
 void gameroms() {
+	cout << hue::light_aqua << "[FlowByte Menu]" << hue::reset << endl;
+	cout << " " << endl;
 	cout << hue::light_red << "[FlowByte]" << hue::reset << " currently games are unavailable, sorry for inconvenience" << endl;
 }
